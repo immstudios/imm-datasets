f934d7129a1705e0b11bc7b88dc0e74c95edeeb9
Martastain <martas@imm.cz>
2017-10-25 20:07:36 +0200

    nicer test

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+import random
+
 from immdatasets import *
 
 data_key = "43074114-b8a0-11e7-a139-005056a0a347"
 data = get_data(data_key)
 
-if data.is_success:
-    for val in data.data:
-        print(data.title, val)
-else:
+if data.is_error:
     print ("Something's wrong")
+    sys.exit(0)
+
+print(data.title, random.choice(data.data))
